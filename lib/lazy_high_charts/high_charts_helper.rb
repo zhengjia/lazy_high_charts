2194c650428708f302290b38b9385edba71c15c1
zhengjia <jiazheng@live.com>
2011-02-14 19:23:03 -0600

    construct available options

--- lib/lazy_high_charts/high_charts_helper.rb
+++ lib/lazy_high_charts/high_charts_helper.rb
@@ -3,30 +3,40 @@
 module LazyHighCharts
   module LayoutHelper
     # ActiveSupport::JSON.unquote_hash_key_identifiers = false
+    def format_options
+      options = []
+      @options.each do |k,v|
+        options << "#{k}: #{v.to_json}"
+      end
+      options.join(',')
+    end
+    private_methods :format_options
+    
     def high_chart(placeholder, object  , &block)
       object.html_options.merge!({:id=>placeholder})
       object.options[:chart][:renderTo] = placeholder
       high_graph(placeholder,object , &block).concat(content_tag("div","", object.html_options))
     end
 
-
     def high_graph(placeholder, object, &block)
+      @options = {
+        "chart"       => object.options[:chart],
+        "title"       => object.options[:title],
+        "legend"      => object.options[:legend],
+        "xAxis"       => object.options[:x_axis],
+        "yAxis"       => object.options[:y_axis],
+        "tooltip"     => object.options[:tooltip],
+        "credits"     => object.options[:credits],
+        "plotOptions" => object.options[:plot_options],
+        "series"      => object.data,
+        "subtitle"    => object.options[:subtitle]
+      }.reject{|k,v| v.nil?}
+      
       graph =<<-EOJS
       <script type="text/javascript">
       jQuery(function() {
             // 1. Define JSON options
-            var options = {
-                          chart: #{object.options[:chart].to_json},
-                                  title: #{object.options[:title].to_json},
-                                  legend: #{object.options[:legend].to_json},
-                                  xAxis: #{object.options[:x_axis].to_json},
-                                  yAxis: #{object.options[:y_axis].to_json},
-                                  tooltip:  #{object.options[:tooltip].to_json},
-                                  credits: #{object.options[:credits].to_json},
-                                  plotOptions: #{object.options[:plot_options].to_json},
-                                  series: #{object.data.to_json},
-                                  subtitle: #{object.options[:subtitle].to_json}
-                          };
+            var options = { #{format_options} };
 
             // 2. Add callbacks (non-JSON compliant)
                                   #{capture(&block) if block_given?}
